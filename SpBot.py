fbbf645d03ba6e18bff65b93f2ffab303fdb4e6a
Arthur Magalhães <arthurmaga@usp.br>
2021-06-09 11:23:29 -0300

    Update SpBot.py

--- SpBot.py
+++ SpBot.py
@@ -18,5 +18,10 @@ agora = datetime.date.today() #Computa o dia de hoje
 fdias_horas = str(ferias - agora) # Diferença entre os dias colocados anteriormente (Tempo para as ferias)
 fdias = fdias_horas[0:3] #para tirar o horario e me dar somente os dias
 
+#### O código abaixo foi uma última atualizacao feita, em que, se o dia for um múltiplo de 10 (50,40,30...) , então o Bot ira twetar com mais animacao
 
-api.update_status("Faltam " + fdias + "dias para as férias da USP") #Tweeta
+if int(fdias) % 10 == 0: #Testando para ver se o dia é um multiplo de 10
+    api.update_status("Faltam " + fdias + "dias para as férias da USP!!!")
+    
+if int(fdias) % 10 != 0: #Testando para ver se o dia é um multiplo de 10
+    api.update_status("Faltam " + fdias + "dias para as férias da USP")
