5fe7ec4a3bab49dd87b9f83ceda2d63363094b9a
Arthur Magalhães <arthurmaga@usp.br>
2021-08-06 11:44:20 -0300

    Update SpBot.py
    Agora estou utilizando uma função .days que deixa tudo mais simples e fácil ainda

--- SpBot.py
+++ SpBot.py
@@ -14,28 +14,26 @@ auth.set_access_token(key, secret)
 
 api = tweepy.API(auth)
 
-ferias = datetime.date(2021,7,31) #Colocar o dia das férias
+
+ferias = datetime.date(2021,8,7) #Colocar o dia das férias
 agora = datetime.date.today() #Computa o dia de hoje
 
-fdias_horas = str(ferias - agora) # Diferença entre os dias colocados anteriormente (Tempo para as ferias)
-fdias  = fdias_horas.split("day")[0] #Tira o horario e me dar somente os dias
+fdias_horas = ferias - agora # Diferença entre os dias colocados anteriormente (Tempo para as ferias)
+fdias = fdias_horas.days  #Agora pegamos somente os dias 
 
-## O try-except foi adicionado para podermos lidar isoladamente com o caso do dia 0, em que a função utilizada não retornava um "0 days" 
-## e portanto quebrava o código , daria também para fazer utilizando o Regex
+#Temos que separa o caso em que falta apenas 1 dia, para poder mudar "dias" para "dia" no tweet
 
-try:
-    if int(fdias) !=1:   #testando para ver se o dia 'diferente de 1'
+if fdias !=1:
 
-        if int(fdias) % 10 == 0:#Testando para ver se o dia é um multiplo de 10
-            api.update_status("Faltam " + fdias + "dias para as férias da USP!!!") #Publicando tweet
+    if fdias % 10 == 0: #Testando para ver se o dia é um multiplo de 10
+        #api.update_status("Faltam " + fdias + "dias para as férias da USP!!!")
+        print("Faltam " + str(fdias) + " dias para as férias da USP!!!")
 
-        if int(fdias) % 10 != 0: #Testando para ver se o dia é um multiplo de 10
-            api.update_status("Faltam " + fdias + "dias para as férias da USP") #Publicando tweet
-            
-    if int(fdias) == 1:  #Rodando o caso especial para quando falta 1 dia
-        api.update_status("Faltam " + fdias + "dia para as férias da USP!!!")   #Lembra de colocar o 'dia' no singular agora!!!
-       
-except: #O caso de quando o dia é 0 e o programa da erro (Lembra de desativar o bot depois disso se não vai rodar denovo a mesma coisa)
-    api.update_status("Começou as férias da USP!!! Descansem muito e cuidem da saúde mental de vocês. Até daqui a 2 semanas :)")
-    
-    
+    if fdias % 10 != 0: #Testando para ver se o dia é um multiplo de 10
+        #api.update_status("Faltam " + fdias + "dias para as férias da USP")
+        print("Faltam " + str(fdias) + " dias para as férias da USP")
+
+if fdias == 1:
+    print("Falta " + str(fdias) + " dia para as férias da USP!!!")
+
+        
